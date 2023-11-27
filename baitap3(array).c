0074220bd57a5b47c6bc0e983be25d9c36afa52c
vuviethoang1906 <148754919+vuviethoang1906@users.noreply.github.com>
2023-11-27 20:55:13 +0700

    Add files via upload

---
+++ baitap3(array).c
@@ -0,0 +1,31 @@
+bài 3 phần bài tập về nhà array
+
+#include <stdio.h>
+
+#define MAX_STUDENTS 50 
+
+int main() {
+    int numStudents, i;
+    float marks[MAX_STUDENTS], sum = 0.0, average;
+
+    printf("Nhap so luong sinh vien trong lop: ");
+    scanf("%d", &numStudents);
+
+    if (numStudents <= 0 || numStudents > MAX_STUDENTS) {
+        printf("So luong sinh vien khong hop le.\n");
+        return 1;
+    }
+
+    for (i = 0; i < numStudents; i++) {
+        printf("Nhap diem cua sinh vien %d: ", i + 1);
+        scanf("%f", &marks[i]);
+   
+        sum += marks[i];
+    }
+
+    average = sum / numStudents;
+   
+    printf("Diem trung binh cua lop la: %.2f\n", average);
+
+    return 0;
+}
